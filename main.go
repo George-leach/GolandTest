4e1916e4af4cd86d02779edd01ee2114f00ffe21
george leach <georgeleach123@gmail.com>
2020-10-11 17:47:08 +0100

    Bit of go routine testing

--- main.go
+++ main.go
@@ -1,61 +1,24 @@
 package main
 
+import "time"
 import "fmt"
 
-/*
-func (c Car)  Print(){
-	fmt.Println(c)
-}
-func swap(ml,m2 *int) {
-	var temp int
-	temp = *m2
-	*m2 = *ml
-	*ml = temp
-}
-
-type Car struct {
-		Name string
-		Miles int
-		ModelNo int
-}
-func todo() {
-	arr := []int{1,2,3,4}
-	fmt.Println(arr)
-}
-func (c Car) GetName() string {
-	return c.Name
-}*/
-
-type Car interface {
-	Drive()
-	Stop()
-}
-type Lambo struct {
-	LamboModel string
-}
-type Chevy struct {
-	ChevyModel string
-}
-
-func NewModel(arg string) Car {
-	return &Lambo{arg}
-}
-func (l *Lambo) Drive() {
-	fmt.Println("Lambo moving")
-	fmt.Println(l.LamboModel)
-}
-func (l *Lambo) Stop() {
-	fmt.Println("Stopping moving")
-
-}
-func (c *Chevy) Drive() {
-	fmt.Println("Chevy moving")
-	fmt.Println(c.ChevyModel)
-}
 func main() {
-	l := Lambo{"gallardo"}
-	c := Chevy{"C369"}
-	l.Drive()
-	l.Stop()
-	c.Drive()
+	go heavy()
+	go superHeavy()
+	fmt.Println("go testing")
+	select {}
+}
+func superHeavy() {
+	for {
+		time.Sleep(time.Second * 2)
+		fmt.Println("super heavy")
+	}
+}
+func heavy() {
+	for {
+		time.Sleep(time.Second * 1)
+		fmt.Println("Hello")
+	}
+
 }
