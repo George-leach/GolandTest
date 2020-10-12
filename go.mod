c93c3385073dd30905b18f9850875dc0acdde024
george leach <georgeleach123@gmail.com>
2020-10-12 01:39:12 +0100

    Bit of go MVC testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GolandTest
 
 go 1.15
+
+require github.com/go-sql-driver/mysql v1.5.0
--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"GolandTest/controller"
+	"GolandTest/model"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	mux := controller.Register()
+	db := model.Connect()
+	defer db.Close()
 	http.ListenAndServe("localhost:3000", mux)
 }
